699a6b3dc8ed910b8e0b403072f2f6299f5d53c5
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2019-11-25 16:41:59 -0700

    work done as of presentation

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@ var $backpack = $('.position-backpack');
 var $lightbulb = $('.position-lightbulb');
 var $trigger1 = $('#trigger1');
 var $introText = $('.position-introText');
-
+var $lightPhoto = $('.position-lightPhoto');
+var $elements = $('.elements');
+var blue = $('.blue');
 var controller = new ScrollMagic.Controller();
 
 var tween = new TimelineMax()
    .add([
       TweenMax.to($arrowwipe, .8, {marginLeft: '102%', width: 0}),
-      TweenMax.to($backpack, .8, {opacity: 1})
+      TweenMax.to($backpack, .8, {opacity: 1}),
+      TweenMax.to($elements, .3, {top: 0}),
+      TweenMax.from($elements, .4, {opacity: 0})
    ]);
 
 var tween2 = new TimelineMax()
@@ -18,9 +22,9 @@ var tween2 = new TimelineMax()
    
 var tween3 = new TimelineMax()
    .add([
-      TweenMax.to('body', .9, {backgroundColor: 'white'})
+      TweenMax.to(blue, .9, {backgroundColor: 'white'})
    ])
-   .add(TweenMax.to('body', .9, {backgroundColor: '#D9632B'}));
+   .add(TweenMax.to(blue, .9, {backgroundColor: '#D9632B'}));
 
 var tween4 = new TimelineMax()
    .add([
@@ -32,32 +36,49 @@ var tween4 = new TimelineMax()
 var tween5 = new TimelineMax()
    .add([
       TweenMax.to($introText, 1, {opacity: 1}),
+      TweenMax.to($lightPhoto, 1, {visibility: 'visible'})
+   ]);
+
+var tween6 = new TimelineMax()
+   .add([
+      TweenMax.to($lightPhoto, 1, {opacity: 1}),
+   ]);
+
+var tween7 = new TimelineMax()
+   .add([
+      TweenMax.to($introText, 1, {opacity: 0}),
+      TweenMax.to($lightPhoto, 1, {opacity: 0}),
+      TweenMax.to('.position-h2', 1, {opacity: 0})
    ]);
 
 
 var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300, tweenChanges: true})
    .setTween(tween)
-   .addIndicators()
    .addTo(controller);
 
 var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300, tweenChanges: true})
    .setTween(tween2)
-   .addIndicators()
    .addTo(controller);
 
 var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 300, tweenChanges: true})
    .setTween(tween3)
-   .addIndicators()
+
    .addTo(controller);
 
 var scene4 = new ScrollMagic.Scene({triggerElement: "#trigger4", duration: 300, tweenChanges: true})
    .setTween(tween4)
-   .addIndicators()
    .addTo(controller);
 
 var scene5 = new ScrollMagic.Scene({triggerElement: "#trigger5", duration: 300, tweenChanges: true})
    .setTween(tween5)
-   .addIndicators()
+   .addTo(controller);
+
+var scene6 = new ScrollMagic.Scene({triggerElement: "#trigger6", duration: 300, tweenChanges: true})
+   .setTween(tween6)
+   .addTo(controller);
+
+var scene7 = new ScrollMagic.Scene({triggerElement: "#trigger7", duration: 300, tweenChanges: true})
+   .setTween(tween7)
    .addTo(controller);
 
 //immediately triggered gsap animations
